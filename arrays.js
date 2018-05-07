d51600458e63750dbd7f953249e387363a8203b8
Choeles Tung <choelestung@gmail.com>
2018-05-07 19:46:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,7 +26,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array,element){
 }
 function removeElementFromBeginningOfArray(array, element) {
   var temp = [array];
-  temp.slice();
+  temp = temp.slice(1);
   return temp;
 } 
 function destructivelyRemoveElementFromEndOfArray(array, element)  {
