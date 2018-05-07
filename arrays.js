59b9a7c72e10acfe5c579cf6d0d245a38bfb7f51
Choeles Tung <choelestung@gmail.com>
2018-05-07 18:20:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred graand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element) {
-  array = [...element];
-  array.push(...element,array);
+function destructivelyAddElementTobeginningOfArray(array, element)  {
+  array = [];
+  array.unshift(element);
   return array;
 }
